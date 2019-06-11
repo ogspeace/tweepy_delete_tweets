b66ecaeea3723857613c06ee26ae82eb5e0913e2
ogspeace <ogremagne@gmail.com>
2019-06-11 17:30:30 +0800

    removed config2

--- delete_tweets_except1.py
+++ delete_tweets_except1.py
@@ -1,18 +1,7 @@
-import os, tweepy, configparser
-
-consumer_key = 'g5BOpHtfJlB505sjHuHyMiBPg' #os.environ['TWPOGSCK']
-consumer_secret = 'sF3NxENnSwVGlEI9SLvDmBPidlfwiYeBVZWIm9Zz4YIuooQaXQ' #os.environ['TWPOGSCS']
-access_token = '400464995-lFoEYqQN9K5U0h23vKwss7lPTOMJYzWBePSuwHHV' #os.environ['TWPOGSAT']
-access_token_secret = 'MpsaYYLmtLeYCMy3f6Qpqjm8YBAnOKOWo4x4SpQiRgblB' #os.environ['TWPOGSATS']
-
-def parse_config(config_file):
-    config = configparser.ConfigParser()
-    config.read(config_file)
-    return config
+import os, tweepy
 
 def oauth_login(config_file):
     cfg = [x.strip() for x in config_file]
-    print(cfg)
     auth = tweepy.OAuthHandler(cfg[0],cfg[1])
     auth.set_access_token(cfg[2],cfg[3])
     return tweepy.API(auth)
@@ -24,9 +13,8 @@ def batch_delete(api):
 
 if __name__ == "__main__":
     dir_name = os.path.dirname(os.path.realpath(__file__))
-    dn = open(dir_name+"/config2.cfg","r")
+    dn = open(dir_name+"/config.cfg","r")
     api = oauth_login(dn.readlines())
-#    print(type(config))
     print(("Authenticated as: %s" % api.me().screen_name))
-#    batch_delete(api)
+    batch_delete(api)
 
